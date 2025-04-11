309f5d87016c166379229e904cb1be50c6327155
Aneesh <boffti@gmail.com>
2025-04-11 12:30:40 -0500

    fix: add API version path to client configuration

--- src/airflow/airflow_client.py
+++ src/airflow/airflow_client.py
@@ -4,7 +4,7 @@ from src.envs import AIRFLOW_HOST, AIRFLOW_PASSWORD, AIRFLOW_USERNAME
 
 # Create a configuration and API client
 configuration = Configuration(
-    host=AIRFLOW_HOST,
+    host=f"{AIRFLOW_HOST}/api/v1",
     username=AIRFLOW_USERNAME,
     password=AIRFLOW_PASSWORD,
 )
